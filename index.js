b759f80a55039ffb7daee2118f61777ae3f8cc2f
codeBunny2022 <119339257+codeBunny2022@users.noreply.github.com>
2022-12-25 17:07:36 +0530

    Its a counting robo..
    Its acts like a gatekeeper.

---
+++ index.js
@@ -0,0 +1,16 @@
+let saveEl = document.getElementById("save-el")
+let countEL = document.getElementById("count-el")
+let count = 0
+function increment() {
+    count += 1
+    countEL.innerText = count
+}
+
+function save() {
+    let countStr = count + " - "
+    saveEl.textContent += countStr
+    countEL.textContent = 0
+    count = 0
+}
+
+
